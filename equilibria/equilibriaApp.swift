130a53985f74f434c24bb0b53be6c2e96cec2ac3
Axl Estevez <axlestevez@gmail.com>
2025-10-02 01:26:40 -0600

    Initial Commit

---
+++ equilibria/equilibriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  equilibriaApp.swift
+//  equilibria
+//
+//  Created by Axl Rodrigo Estevez NIeto on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct equilibriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
